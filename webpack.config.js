2ba28deee43802069519ec8a2dd07d9265f1f252
George Campbell <campbell.goe@gmail.com>
2022-06-04 01:36:07 +0100

    game of life

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,10 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
-  entry: "./bootstrap.js",
+  entry: "./wasm-dynamic-import.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "wasm-dynamic-import.js",
   },
   mode: "development",
   plugins: [
